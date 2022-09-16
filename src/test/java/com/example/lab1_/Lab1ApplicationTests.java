8df384f3263be0f75b62dedfa94d3e5dde1927a0
nafis <nafu22@gmail.com>
2022-09-16 06:35:08 -0700

    Initial commit

---
+++ src/test/java/com/example/lab1_/Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
